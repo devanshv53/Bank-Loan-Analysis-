b07a8a39aad734de19c2de40344213bc524bc0e7
Devansh Verma <136915115+devanshv53@users.noreply.github.com>
2025-09-25 01:11:55 +0530

    Test_SQLQuery.sql

--- SQLQuery111111.sql
+++ SQLQuery111111.sql
@@ -1,49 +1,121 @@
-SELECT * FROM bank_loan_data
+BANK LOAN REPORT QUERY DOCUMENT
+	•	BANK LOAN REPORT | SUMMARY
+KPI’s:
+Total Loan Applications
+SELECT COUNT(id) AS Total_Applications FROM bank_loan_data
 
-SELECT SUM(total_payment) AS MTD_Total_Amount_received FROM bank_loan_data
-WHERE MONTH(issue_date) = 12 AND YEAR(issue_date) = 2021
+MTD Loan Applications
+SELECT COUNT(id) AS Total_Applications FROM bank_loan_data
+WHERE MONTH(issue_date) = 12
 
-SELECT SUM(total_payment) AS PMTD_Total_Amount_received FROM bank_loan_data
-WHERE MONTH(issue_date) = 11 AND YEAR(issue_date) = 2021
+PMTD Loan Applications SELECT COUNT(id) AS Total_Applications FROM bank_loan_data
+WHERE MONTH(issue_date) = 11
 
 
-SELECT ROUND(AVG(dti),4)*100 AS PMTD_Avg_DTI FROM bank_loan_data
-WHERE MONTH(issue_date) = 11 AND YEAR(issue_date) = 2021
+Total Funded Amount
+SELECT SUM(loan_amount) AS Total_Funded_Amount FROM bank_loan_data
 
-SELECT loan_status FROM bank_loan_data
+MTD Total Funded Amount
+SELECT SUM(loan_amount) AS Total_Funded_Amount FROM bank_loan_data
+WHERE MONTH(issue_date) = 12
 
+PMTD Total Funded Amount
+SELECT SUM(loan_amount) AS Total_Funded_Amount FROM bank_loan_data
+WHERE MONTH(issue_date) = 11
+
+
+
+Total Amount Received
+SELECT SUM(total_payment) AS Total_Amount_Collected FROM bank_loan_data
+
+MTD Total Amount Received
+SELECT SUM(total_payment) AS Total_Amount_Collected FROM bank_loan_data
+WHERE MONTH(issue_date) = 12
+
+PMTD Total Amount Received
+SELECT SUM(total_payment) AS Total_Amount_Collected FROM bank_loan_data
+WHERE MONTH(issue_date) = 11
+
+
+Average Interest Rate
+SELECT AVG(int_rate)*100 AS Avg_Int_Rate FROM bank_loan_data
+
+MTD Average Interest
+SELECT AVG(int_rate)*100 AS MTD_Avg_Int_Rate FROM bank_loan_data
+WHERE MONTH(issue_date) = 12
+
+PMTD Average Interest
+SELECT AVG(int_rate)*100 AS PMTD_Avg_Int_Rate FROM bank_loan_data
+WHERE MONTH(issue_date) = 11
+
+
+
+
+
+
+Avg DTI
+SELECT AVG(dti)*100 AS Avg_DTI FROM bank_loan_data
+
+MTD Avg DTI
+SELECT AVG(dti)*100 AS MTD_Avg_DTI FROM bank_loan_data
+WHERE MONTH(issue_date) = 12
+
+PMTD Avg DTI
+SELECT AVG(dti)*100 AS PMTD_Avg_DTI FROM bank_loan_data
+WHERE MONTH(issue_date) = 11
+
+
+
+GOOD LOAN ISSUED
+Good Loan Percentage
 SELECT
     (COUNT(CASE WHEN loan_status = 'Fully Paid' OR loan_status = 'Current' THEN id END) * 100.0) / 
 	COUNT(id) AS Good_Loan_Percentage
 FROM bank_loan_data
 
+Good Loan Applications
 SELECT COUNT(id) AS Good_Loan_Applications FROM bank_loan_data
 WHERE loan_status = 'Fully Paid' OR loan_status = 'Current'
 
+Good Loan Funded Amount
 SELECT SUM(loan_amount) AS Good_Loan_Funded_amount FROM bank_loan_data
 WHERE loan_status = 'Fully Paid' OR loan_status = 'Current'
 
+
+Good Loan Amount Received
 SELECT SUM(total_payment) AS Good_Loan_amount_received FROM bank_loan_data
 WHERE loan_status = 'Fully Paid' OR loan_status = 'Current'
 
+
+BAD LOAN ISSUED
+Bad Loan Percentage
 SELECT
     (COUNT(CASE WHEN loan_status = 'Charged Off' THEN id END) * 100.0) / 
 	COUNT(id) AS Bad_Loan_Percentage
 FROM bank_loan_data
 
+Bad Loan Applications
 SELECT COUNT(id) AS Bad_Loan_Applications FROM bank_loan_data
 WHERE loan_status = 'Charged Off'
 
+Bad Loan Funded Amount
 SELECT SUM(loan_amount) AS Bad_Loan_Funded_amount FROM bank_loan_data
 WHERE loan_status = 'Charged Off'
 
+Bad Loan Amount Received
 SELECT SUM(total_payment) AS Bad_Loan_amount_received FROM bank_loan_data
 WHERE loan_status = 'Charged Off'
 
 
-SELECT
+
+
+
+
+
+LOAN STATUS
+	SELECT
         loan_status,
-        COUNT(id) AS Total_Loan_applications,
+        COUNT(id) AS LoanCount,
         SUM(total_payment) AS Total_Amount_Received,
         SUM(loan_amount) AS Total_Funded_Amount,
         AVG(int_rate * 100) AS Interest_Rate,
@@ -53,7 +125,8 @@ SELECT
     GROUP BY
         loan_status
 
-		SELECT 
+
+SELECT 
 	loan_status, 
 	SUM(total_payment) AS MTD_Total_Amount_Received, 
 	SUM(loan_amount) AS MTD_Total_Funded_Amount 
@@ -61,6 +134,21 @@ FROM bank_loan_data
 WHERE MONTH(issue_date) = 12 
 GROUP BY loan_status
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+	•	BANK LOAN REPORT | OVERVIEW
+MONTH
 SELECT 
 	MONTH(issue_date) AS Month_Munber, 
 	DATENAME(MONTH, issue_date) AS Month_name, 
@@ -71,6 +159,20 @@ FROM bank_loan_data
 GROUP BY MONTH(issue_date), DATENAME(MONTH, issue_date)
 ORDER BY MONTH(issue_date)
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+STATE
 SELECT 
 	address_state AS State, 
 	COUNT(id) AS Total_Loan_Applications,
@@ -81,6 +183,7 @@ GROUP BY address_state
 ORDER BY address_state
 
 
+TERM
 SELECT 
 	term AS Term, 
 	COUNT(id) AS Total_Loan_Applications,
@@ -90,6 +193,8 @@ FROM bank_loan_data
 GROUP BY term
 ORDER BY term
 
+
+EMPLOYEE LENGTH
 SELECT 
 	emp_length AS Employee_Length, 
 	COUNT(id) AS Total_Loan_Applications,
@@ -99,7 +204,7 @@ FROM bank_loan_data
 GROUP BY emp_length
 ORDER BY emp_length
 
-
+PURPOSE
 SELECT 
 	purpose AS PURPOSE, 
 	COUNT(id) AS Total_Loan_Applications,
@@ -110,6 +215,7 @@ GROUP BY purpose
 ORDER BY purpose
 
 
+HOME OWNERSHIP
 SELECT 
 	home_ownership AS Home_Ownership, 
 	COUNT(id) AS Total_Loan_Applications,
@@ -119,6 +225,10 @@ FROM bank_loan_data
 GROUP BY home_ownership
 ORDER BY home_ownership
 
+
+Note: We have applied multiple Filters on all the dashboards. You can check the results for the filters as well by modifying the query and comparing the results.
+For e.g
+See the results when we hit the Grade A in the filters for dashboards.
 SELECT 
 	purpose AS PURPOSE, 
 	COUNT(id) AS Total_Loan_Applications,
@@ -128,5 +238,3 @@ FROM bank_loan_data
 WHERE grade = 'A'
 GROUP BY purpose
 ORDER BY purpose
-
-
